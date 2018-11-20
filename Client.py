04ffaee8d1c2dd6bb10aef85d22bf9d5aae2422c
Aaron Christson <neo@neo-ubuntu.berea.edu>
2018-11-19 19:10:31 -0500

    Cleaned up the code.

--- Client.py
+++ Client.py
@@ -19,37 +19,22 @@ try:
 except socket.gaierror:
     print ('Hostname could not be resolved. Exiting ...')
     sys.exit()
-# print ('Ip address of ' + host + ' is ' + remote_ip)
 
 s.connect((remote_ip , port))
-# print ('Socket connected to ' + host + ' on ip ' + remote_ip)
 num = s.recv(1024).decode('ASCII')
-# print(num)
 intro = s.recv(1024)
 intro = intro.decode('ASCII')
 print(intro)
-# print("intro")
 print("\n")
-# while True:
 available_texts = []
 for _ in range(int(num)):
     text = s.recv(1024)
     text = text.decode('ASCII')
-    # if text == "done":
-    #     break
     available_texts.append(text)
     print(text)
 
 print("\n")
 
-    # break
-# print(text)
-# print("here")
-# text = text.decode('ASCII')
-# # if not text:
-# #     break
-# print(text)
-# print("done")
 filename = str(input('Which file would you like to download?: '))
 while not filename or filename not in available_texts:
     filename = str(input('Please enter a file name from the available list: '))
@@ -69,10 +54,8 @@ while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         load_bar += "#"
         print(load_bar)
-        # print('got new line')
         time.sleep(0.2)
     elif reply == 'fin':
-        # print('ok finished')
         os.system('cls' if os.name == 'nt' else 'clear')
         load_bar += "]"
         print(load_bar)
--- Server.py
+++ Server.py
@@ -3,7 +3,6 @@ import sys
 import time
 
 def client(conn):
-    # print(b'Welcome to the Server.')
     texts = ["Asyoulikeit.txt","HenryV.txt","JuliusCaesar.txt","Macbeth.txt","Othello.txt"]
     num_texts = str(len(texts))
     conn.sendall(num_texts.encode('ASCII'))
@@ -13,17 +12,13 @@ def client(conn):
     for text in texts:
         time.sleep(0.01)
         conn.sendall(text.encode('ASCII'))
-        # conn.sendall("\n".encode('ASCII'))
-    # .conn.sendall("done".encode('ASCII'))
-    # print("done")
+
     filename = conn.recv(1024)
     filename = filename.decode('ASCII')
     while not filename:
         filename = conn.recv(1024).decode('ASCII')
 
     file = open(filename,'r')
-    # conn.sendall(filename.encode('ASCII'))
-    # print("sent filename")
     while True:
         reply = conn.recv(1024)
         reply = reply.decode('ASCII')
@@ -37,7 +32,6 @@ def client(conn):
             break
         line = read.encode('ASCII','ignore')
         conn.sendall(line)
-        # print("sent new line")
         time.sleep(0.2)
 
     file.close()
@@ -49,11 +43,9 @@ listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
-# print ('serving HTTP on port ', PORT)
 
 while True:
     client_connection, client_address = listen_socket.accept()
-    # print ('server connected to ' + client_address[0] + ':' + str(client_address[1]))
     client(client_connection)
     break
 listen_socket.close()
