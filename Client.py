c2bdab251ded847a854ed46824a4322b39c46134
Aaron Christson <neo@neo-ubuntu.berea.edu>
2018-12-05 19:20:25 -0500

    Added comments to the code

--- Client.py
+++ Client.py
@@ -21,13 +21,13 @@ except socket.gaierror:
     sys.exit()
 
 s.connect((remote_ip , port))
-num = s.recv(1024).decode('ASCII')
-intro = s.recv(1024)
+num = s.recv(1024).decode('ASCII') # receives the number of texts from the server
+intro = s.recv(1024)               # receives the intro message from the server
 intro = intro.decode('ASCII')
-print(intro)
+print(intro)                       # prints the intro message
 print("\n")
 available_texts = []
-for _ in range(int(num)):
+for _ in range(int(num)):          # receives the texts that are in the server and prints it out for the client to see
     text = s.recv(1024)
     text = text.decode('ASCII')
     available_texts.append(text)
@@ -35,46 +35,43 @@ for _ in range(int(num)):
 
 print("\n")
 
-filename = str(input('Which file would you like to download?: '))
+filename = str(input('Which file would you like to download?: '))   # asks the usesr what file they want to download
 while not filename or filename not in available_texts:
-    filename = str(input('Please enter a file name from the available list: '))
+    filename = str(input('Please enter a file name from the available list: '))  # if the file entered was not one of the files received from the server the user is asked to enter a new one
 
-s.sendall(filename.encode('ASCII'))
-num_lines = s.recv(1024).decode('ASCII')
-num_lines = int(num_lines)
+s.sendall(filename.encode('ASCII'))                 # sends the file chosen to the server
+num_lines = s.recv(1024).decode('ASCII')            # receives the number of lines in the chosen file
+num_lines = int(num_lines)                          # to calculate the percentage of the file that has been transfered
 filename = filename.split('.')
-file = open(filename[0]+"-copy.txt",'w')
+file = open(filename[0]+"-copy.txt",'w')            # creates a new file to copy the file in the server
 s.sendall("Y".encode('ASCII'))
 
-load_bar = "["
+load_bar = "["                                      # initiates the load bar
 #for i in range(int(num_lines)):
     #load_bar += " "
 
 z = 0
 while True:
-    reply = s.recv(1024)
-    reply = reply.decode('ASCII')
-    if reply != 'fin':
-        file.write(reply)
-        s.sendall("Y".encode('ASCII'))
-        os.system('cls' if os.name == 'nt' else 'clear')
-        load_bar += "#"
-        perc = (z/num_lines)*100
-        print(perc)
+    reply = s.recv(1024)                                     # receives a line from the file that is being transferred
+    reply = reply.decode('ASCII')                            # decodes the line
+    if reply != 'fin':                                       # keeps writing to the file if the last line has not been received
+        file.write(reply)                                    
+        s.sendall("Y".encode('ASCII'))                       # sends a Y to the server to acknowledge the received line
+        os.system('cls' if os.name == 'nt' else 'clear')     
+        load_bar += "#"                                      # adds to the loadbar
+        perc = (z/num_lines)*100                             # calculates the percentage of the file received
         print(load_bar,str(perc)+"%")
-        #print(z)
         time.sleep(0.2)
         z += 1
     elif reply == 'fin':
-        os.system('cls' if os.name == 'nt' else 'clear')
+        os.system('cls' if os.name == 'nt' else 'clear')     # clears the console to show the animation for the load bar
         load_bar += "]"
         perc = (z/num_lines)*100
-        print(load_bar,str(perc)+"%")
-        print(z)
+        print(load_bar,str(perc)+"%")                        # prints the percentage  
         print("File download complete")
         break
     else:
-        s.sendall("N".encode('ASCII'))
+        s.sendall("N".encode('ASCII'))                       # sends an N to the server to tell the server that the line was not received
         print("didnt get it")
-file.close()
-s.close()
+file.close()                                                 # closes the file 
+s.close()                                                    # closes the connection
--- Server.py
+++ Server.py
@@ -3,41 +3,41 @@ import sys
 import time
 
 def client(conn):
-    texts = ["Asyoulikeit.txt","HenryV.txt","JuliusCaesar.txt","Macbeth.txt","Othello.txt"]
-    num_texts = str(len(texts))
-    conn.sendall(num_texts.encode('ASCII'))
+    texts = ["Asyoulikeit.txt","HenryV.txt","JuliusCaesar.txt","Macbeth.txt","Othello.txt"]    # the available texts for transfer
+    num_texts = str(len(texts))                                             
+    conn.sendall(num_texts.encode('ASCII'))                                                  # sends the number of texts to the client
     time.sleep(0.01)
-    conn.sendall("Welcome to the Server. Here are the files that are available for download.".encode('ASCII'))
+    conn.sendall("Welcome to the Server. Here are the files that are available for download.".encode('ASCII'))  # sends the intro message
     time.sleep(0.01)
-    for text in texts:
+    for text in texts:                                            # sends the available texts to the client 
         time.sleep(0.01)
         conn.sendall(text.encode('ASCII'))
 
-    filename = conn.recv(1024)
+    filename = conn.recv(1024)                                    # receives the chosen filename
     filename = filename.decode('ASCII')
     while not filename:
         filename = conn.recv(1024).decode('ASCII')
-    num_lines = sum(1 for line in open(filename))
-    num_lines = str(num_lines)
-    conn.sendall(num_lines.encode('ASCII'))
-    file = open(filename,'r')
+    num_lines = sum(1 for line in open(filename))                 # counts the lines in the chosen file
+    num_lines = str(num_lines)                   
+    conn.sendall(num_lines.encode('ASCII'))                       # sends the number of lines to the client
+    file = open(filename,'r')                                     # opens the file for reading
     while True:
-        reply = conn.recv(1024)
+        reply = conn.recv(1024)                                   # receives the Y or N from the client
         reply = reply.decode('ASCII')
-        while reply == 'N':
+        while reply == 'N':                                       # if an N is received then the line is sent again 
             conn.sendall(line)
             print("sending again")
-        read = file.readline()
+        read = file.readline()                                    # if a Y is received then a new line is read 
         if not read:
-            conn.sendall("fin".encode('ASCII'))
+            conn.sendall("fin".encode('ASCII'))                   # if there is no new line, then the download is complete and the code breaks
             print("Download Complete")
             break
-        line = read.encode('ASCII','ignore')
-        conn.sendall(line)
+        line = read.encode('ASCII','ignore')  
+        conn.sendall(line)                                        # the line is sent to the client
         time.sleep(0.2)
 
-    file.close()
-    conn.close()
+    file.close()                                                  # the file is closed 
+    conn.close()                                                  # the connection is closed
 
 HOST, PORT = '', 8234
 
